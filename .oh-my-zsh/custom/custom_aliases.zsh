3e154bc93d4196888dcb3090a4feeed4ae508464
David Losert <david.losert@gmx.de>
2021-03-08 09:26:00 +0100

    Remove unecessary entries

--- .oh-my-zsh/custom/custom_aliases.zsh
+++ .oh-my-zsh/custom/custom_aliases.zsh
@@ -1,17 +1,9 @@
-alias thd="cd /home/davidlosert/Projects/VDMA/vdma-themis/"
-alias vdma="cd /home/davidlosert/Projects/VDMA"
-alias tng="cd /home/davidlosert/Projects/VDMA/vdma-themis-ng-client"
-
 alias dp:home="xrandr --output VGA-1 --left-of VGA-0"
 alias dp:work="xrandr --output DP2-2 --right-of eDP1 && xrandr --output DP2-2 --auto && xrandr --output DP2-1 --right-of DP2-2 && xrandr --output DP2-2 --auto"
-
-alias tngapi="cd /home/davidlosert/Projects/VDMA/vdma-themis-ng-server/api"
 
 alias dp:home="xrandr --output eDP1 --left-of DP1 && xrandr --output DP1 --mode 2560x1440"
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias au='sudo apt-get update -y'
 alias aug='sudo apt-get upgrade -y'
-alias tngs='cd ~/Projects/VDMA/vdma-themis-ng-server/'
-alias ic='cd Projects/VDMA/intrexx/portalManager/client/bin/linux'
 alias o='xdg-open'
